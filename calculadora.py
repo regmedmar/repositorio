fcf784325573e5d4bc1e70943efb7104506ccb61
Regina <regina.medina@educa.madrid.org>
2025-04-06 21:56:45 +0200

    error e el texto de menu

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,7 @@ def main():
     
     seguir=True
     while (seguir):
-        print("Calculadora de operaciones sencillas P2")
+        print("Calculadora de operaciones muy básicas sencillas P2")
         print("1.Sumar")
         print("2.Restar")
         print("3.Multiplicar")

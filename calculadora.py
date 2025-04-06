0f7cedae44b23ed97fe6f7f671bccf7e9a5e6f0c
Regina <regina.medina@educa.madrid.org>
2025-04-06 20:33:52 +0200

    commit menu

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,16 @@
 def main():
-
-    print ("Calculadora de operaciones sencillas P2")
-
-if __name__=="__main__":
-    main()
-
     
+    seguir=True
+    while (seguir):
+        print("Calculadora de operaciones sencillas P2")
+        print("1.Sumar")
+        print("2.Restar")
+        print("3.Multiplicar")
+        print("4.Dividir")
+        print("5.Salir")
+        opcion=input("Elija una opción")
+        print ("Opción elegida", opcion)
+        seguir=opcion!="5"
+
+    if __name__=="__main__":
+        main()

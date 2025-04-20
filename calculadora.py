8caa7d796d19e2ab48db1343be87418a90bff48b
Regina <regina.medina@educa.madrid.org>
2025-04-21 00:02:35 +0200

    division

--- calculadora.py
+++ calculadora.py
@@ -25,6 +25,11 @@ def main():
              num2= float(input ("Segundo num 2: "))
              resultado=num1 * num2
              print ("Resultado", resultado)
+        if opcion == "4":
+             num1= float(input ("Primer num 1: "))
+             num2= float(input ("Segundo num 2: "))
+             resultado=num1 / num2
+             print ("Resultado", resultado)
         seguir=opcion!="5"
 if __name__=="__main__":
-        main()
+     main()
